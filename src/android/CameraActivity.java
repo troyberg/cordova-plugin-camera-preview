90409cf09202a1f4fa289ec74cb34031978b36f1
troyberg <troy.cflweb@gmail.com>
2017-12-05 15:52:30 -0500

    Update CameraActivity.java

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -33,7 +33,6 @@ import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.support.media.ExifInterface;
 
 import org.apache.cordova.LOG;
 
